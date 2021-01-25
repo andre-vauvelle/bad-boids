994bf1f24d95f27e2a770b5ba95f9bedbf573a4d
Andre Vauvelle <vauvelleandre@gmail.com>
2021-01-25 17:32:15 +0000

    add boids class init

--- boids.py
+++ boids.py
@@ -6,57 +6,70 @@ for use as an exercise on refactoring.
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import os
-import random
+import numpy as np
 import yaml
 
+
 # Deliberately terrible code for teaching purposes
-boids = yaml.load(open(os.path.join(os.path.dirname(__file__), 'init_boids.yaml')), Loader=yaml.FullLoader)
-boids_x = [random.uniform(boids['position']['x']['lower'], boids['position']['x']['upper']) for _ in
-range(boids['n_sample'])]
-boids_y = [random.uniform(boids['position']['y']['lower'], boids['position']['y']['upper']) for _ in
-range(boids['n_sample'])]
-boid_x_velocities = [random.uniform(boids['velocity']['x']['lower'], boids['velocity']['x']['upper']) for _ in
-range(boids['n_sample'])]
-boid_y_velocities = [random.uniform(boids['velocity']['y']['lower'], boids['velocity']['y']['upper']) for _ in
-range(boids['n_sample'])]
-boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+
+class Boids:
+    def __init__(self, xs, ys, xvs, yvs):
+        self.xs = xs
+        self.ys = ys
+        self.xvs = xvs
+        self.yvs = yvs
+
+
+def init_boids():
+    b_init = yaml.load(open(os.path.join(os.path.dirname(__file__), 'init_boids.yaml')), Loader=yaml.FullLoader)
+    boids_x = np.random.uniform(b_init['position']['x']['lower'], b_init['position']['x']['upper'],
+                                size=b_init['n_sample'])
+    boids_y = np.random.uniform(b_init['position']['y']['lower'], b_init['position']['y']['upper'],
+                                size=b_init['n_sample'])
+    boid_x_velocities = np.random.uniform(b_init['velocity']['x']['lower'], b_init['velocity']['x']['upper'],
+                                          size=b_init['n_sample'])
+    boid_y_velocities = np.random.uniform(b_init['velocity']['y']['lower'], b_init['velocity']['y']['upper'],
+                                          size=b_init['n_sample'])
+    return Boids(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+
+
+boids = init_boids()
 
 
 def update_boids(boids):
-    xs, ys, xvs, yvs = boids
     # Fly towards the middle
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            xvs[i] = xvs[i] + (xs[j] - xs[i]) * 0.01 / len(xs)
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            yvs[i] = yvs[i] + (ys[j] - ys[i]) * 0.01 / len(xs)
+    for i in range(len(boids.xs)):
+        for j in range(len(boids.xs)):
+            boids.xvs[i] = boids.xvs[i] + (boids.xs[j] - boids.xs[i]) * 0.01 / len(boids.xs)
+    for i in range(len(boids.xs)):
+        for j in range(len(boids.xs)):
+            boids.yvs[i] = boids.yvs[i] + (boids.ys[j] - boids.ys[i]) * 0.01 / len(boids.xs)
     # Fly away from nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
-                xvs[i] = xvs[i] + (xs[i] - xs[j])
-                yvs[i] = yvs[i] + (ys[i] - ys[j])
+    for i in range(len(boids.xs)):
+        for j in range(len(boids.xs)):
+            if (boids.xs[j] - boids.xs[i]) ** 2 + (boids.ys[j] - boids.ys[i]) ** 2 < 100:
+                boids.xvs[i] = boids.xvs[i] + (boids.xs[i] - boids.xs[j])
+                boids.yvs[i] = boids.yvs[i] + (boids.ys[i] - boids.ys[j])
     # Try to match speed with nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
-                xvs[i] = xvs[i] + (xvs[j] - xvs[i]) * 0.125 / len(xs)
-                yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * 0.125 / len(xs)
+    for i in range(len(boids.xs)):
+        for j in range(len(boids.xs)):
+            if (boids.xs[j] - boids.xs[i]) ** 2 + (boids.ys[j] - boids.ys[i]) ** 2 < 10000:
+                boids.xvs[i] = boids.xvs[i] + (boids.xvs[j] - boids.xvs[i]) * 0.125 / len(boids.xs)
+                boids.yvs[i] = boids.yvs[i] + (boids.yvs[j] - boids.yvs[i]) * 0.125 / len(boids.xs)
     # Move according to velocities
-    for i in range(len(xs)):
-        xs[i] = xs[i] + xvs[i]
-        ys[i] = ys[i] + yvs[i]
+    for i in range(len(boids.xs)):
+        boids.xs[i] = boids.xs[i] + boids.xvs[i]
+        boids.ys[i] = boids.ys[i] + boids.yvs[i]
 
 
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(boids[0], boids[1])
+scatter = axes.scatter(boids.xs, boids.ys)
 
 
 def animate(frame):
     update_boids(boids)
-    scatter.set_offsets(list(zip(boids[0], boids[1])))
+    scatter.set_offsets(list(zip(boids.xs, boids.ys)))
 
 
 anim = animation.FuncAnimation(figure, animate,
--- record_fixture.py
+++ record_fixture.py
@@ -2,9 +2,9 @@ import yaml
 import boids
 from copy import deepcopy
 
-before = deepcopy(boids.boids)
-boids.update_boids(boids.boids)
-after = boids.boids
+before = deepcopy(boids.b_init)
+boids.update_boids(boids.b_init)
+after = boids.b_init
 fixture = {"before": before, "after": after}
 fixture_file = open("fixture.yml", 'w')
 fixture_file.write(yaml.dump(fixture))
